d8c746a33f2853656e479b2ec8003a9fcc9e97f0
Vuong Le <vuongle.dev@gmail.com>
2024-07-05 08:32:21 +0700

    commit

--- redeploy.sh
+++ redeploy.sh
@@ -1,5 +1,5 @@
 cd ../Appliance-Shop-API
-git remote update -q
+git remote update --quiet
 UPSTREAM=${1:-'@{u}'}
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
@@ -8,9 +8,9 @@ BASE=$(git merge-base @ "$UPSTREAM")
 if [ $LOCAL = $REMOTE ]; then
     echo "Up-to-date"
 else
-    git reset --hard -q
+    git reset --hard --quiet
     echo "Pulling from git..."
-    git pull -q
+    git pull --quiet
     echo "Rebuilding..."
     /www/server/nodejs/v18.20.3/bin/yarn build
 fi
